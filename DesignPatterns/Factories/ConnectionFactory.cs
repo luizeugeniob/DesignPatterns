c74ac7e7c95d5bc583e19fe95574e719e37957f9
Luiz Eugênio Barbieri <luizeugeniobarbieri@gmail.com>
2021-06-22 21:13:57 -0300

    Implementação de um exemplo de Factory.

---
+++ DesignPatterns/Factories/ConnectionFactory.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace DesignPatterns
+{
+    public class ConnectionFactory
+    {
+        public IDbConnection GetConnection()
+        {
+            IDbConnection connection = new SqlConnection
+            {
+                ConnectionString = "User Id=root;Password=;Server=localhost;Database=DB"
+            };
+            connection.Open();
+            return connection;
+        }
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,30 +1,13 @@
-﻿using System;
-
-namespace DesignPatterns
+﻿namespace DesignPatterns
 {
     internal static class Program
     {
         private static void Main(string[] args)
         {
-            var invoiceBuilder = new InvoiceBuilder();
+            var conexao = new ConnectionFactory().GetConnection();
 
-            invoiceBuilder
-                .ForCompany("Empresa 123")
-                .WithCnpj("00.000.000/0001-00")
-                .WithItem(new InvoiceItem("Item 1", 150.50))
-                .WithItem(new InvoiceItem("Item 2", 230.90))
-                .OnDate(new DateTime(1994, 6, 5))
-                .WithObservation("Entrega urgente.");
-
-            invoiceBuilder.AddAction(new Multiplier(5));
-
-            var invoice = invoiceBuilder.Build();
-
-            Console.WriteLine(invoice.GrossAmount);
-            Console.WriteLine(invoice.Tax);
-            Console.WriteLine(invoice.IssueDate);
-
-            Console.ReadLine();
+            var comando = conexao.CreateCommand();
+            comando.CommandText = "SELECT * FROM SomeTable";
         }
     }
 }
