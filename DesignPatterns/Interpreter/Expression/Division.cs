c06d2717d1046f866e24b9ec6315ebca827e2a66
Luiz Eugênio Barbieri <luizeugeniobarbieri@gmail.com>
2021-06-24 21:38:41 -0300

    Implementação de novas expressões para o ExpressionInterpreter.

---
+++ DesignPatterns/Interpreter/Expression/Division.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPatterns
+{
+    public class Division : IExpression
+    {
+        private readonly IExpression left;
+        private readonly IExpression right;
+
+        public Division(IExpression left, IExpression right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public int Evaluate()
+        {
+            return left.Evaluate() / right.Evaluate();
+        }
+    }
+}
---
+++ DesignPatterns/Interpreter/Expression/Multiplication.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPatterns
+{
+    public class Multiplication : IExpression
+    {
+        private readonly IExpression left;
+        private readonly IExpression right;
+
+        public Multiplication(IExpression left, IExpression right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public int Evaluate()
+        {
+            return left.Evaluate() * right.Evaluate();
+        }
+    }
+}
